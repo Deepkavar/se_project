5da459d1b99e6a8d4368f9b62dfde33bb0074eca
Deepkavar <55187344+Deepkavar@users.noreply.github.com>
2022-04-07 18:14:03 +0530

    six commit

--- index.php
+++ index.php
@@ -81,6 +81,7 @@ alert('User already registered. Please try again');
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <h1>change</h1>
         <title>Clinic Appointment Application</title>
         <!-- Bootstrap -->
         <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet"> -->
